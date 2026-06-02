c3f319091c6980253c425d359667ff41a2feca75
mandcoder <anderssonmaarcus@icloud.com>
2026-06-02 09:36:30 +0000

    add table_config
    - Enable column mapping for special characters in column names
    - Set minimum reader/writer versions required for column mapping

---
+++ marathos_Maarcus_Andersson/utils/table_config.py
@@ -0,0 +1,8 @@
+# Default table-properties for all Delta tables in the pipeline
+
+DEFAULT_TABLE_PROPERTIES = {
+    "delta.columnMapping.mode": "name",
+    "delta.minReaderVersion" : "2",
+    "delta.minWriterVersion" : "5"
+}
+
